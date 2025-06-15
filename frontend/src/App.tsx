a78c26e207900b455cb94716bc91198d8945a2e1
Pdave <ferdinandpdave@gmail.com>
2025-06-14 22:30:42 -0400

    feat: add NotFound page and update routing in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import Index from "./pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <Index />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
